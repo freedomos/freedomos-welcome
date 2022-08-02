4849375d36be3cbd45c89ddae0df93db9c059616
frazzlesgrant1994 <frazer_grant@outlook.com>
2022-08-02 20:15:15 +0100

    update

--- usr/share/freedomos-welcome/GUI.py
+++ usr/share/freedomos-welcome/GUI.py
@@ -16,7 +16,7 @@ username = getpass.getuser()
 if DEBUG:
     user = username
 else:
-    user = "liveuser"
+    user = "live"
 
 Settings = home + "/.config/freedomos-welcome/settings.conf"
 Skel_Settings = "/etc/skel/.config/freedomos-welcome/settings.conf"
